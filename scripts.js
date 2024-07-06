af3f74daba0d3c3263df95b5f0898766774d2070
Kevin <kyu065@uottawa.ca>
2024-07-06 10:00:34 -0400

    c3

---
+++ scripts.js
@@ -0,0 +1,35 @@
+document.getElementById('signupButton').addEventListener('click', function() {
+  window.location.href = 'signup.html';
+});
+
+document.getElementById('loginButton').addEventListener('click', function() {
+  window.location.href = 'login.html';
+});
+
+document.getElementById('showSignupForm').addEventListener('click', function() {
+  window.location.href = 'signup.html';
+});
+
+document.getElementById('showLoginForm').addEventListener('click', function() {
+  window.location.href = 'login.html';
+});
+
+document.getElementById('playNowButton').addEventListener('click', function() {
+  window.location.href = 'games.html';
+});
+
+document.getElementById('okButton').addEventListener('click', function() {
+  window.location.href = 'index.html';
+});
+
+document.getElementById('dealsLink').addEventListener('click', function() {
+  window.location.href = 'deals.html';
+});
+
+document.getElementById('gamesLink').addEventListener('click', function() {
+  window.location.href = 'games.html';
+});
+
+document.getElementById('purchaseLink').addEventListener('click', function() {
+  window.location.href = 'purchase.html';
+});
